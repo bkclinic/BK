c8c9581b4f2762feeee9377314020e4f1a1c0837
bkclinic <bkclinic.cc@gmail.com>
2026-05-22 23:12:54 +0500

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Firebase Console → Project settings → Your apps
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyB45bc1AXFP8k9Jv1ccH8OrpGFbSsWw5tI",
+  authDomain: "bkclinic-2d141.firebaseapp.com",
+  projectId: "bkclinic-2d141",
+  storageBucket: "bkclinic-2d141.firebasestorage.app",
+  messagingSenderId: "798161083323",
+  appId: "1:798161083323:web:3c6febe6e603329f57c29e",
+  measurementId: "G-603S33C8K0"
+};
